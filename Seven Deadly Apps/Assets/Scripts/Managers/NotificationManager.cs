bba433cfc01d595e7e888a5e4964441061711f42
MessyPython86 <Jjlenton27@gmail.com>
2024-02-15 23:42:27 +0000

    Branch Test

---
+++ Seven Deadly Apps/Assets/Scripts/Managers/NotificationManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+Used to manage the ui for the "notifications"
+*/
+
+public class NotificationManager : Singleton{
+    public TextMeshPro textBox;
+    public Animation animator;
+
+    public void SendNotification(string msg){
+        textBox.text = msg;
+        animator.Play("Notification Arrive");
+    }
+}
