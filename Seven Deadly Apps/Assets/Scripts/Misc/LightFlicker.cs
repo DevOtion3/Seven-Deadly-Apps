e23a5d8115b984cf89a8e31e6420648e298793d2
Tony Leva <58071254+DevOtion3@users.noreply.github.com>
2024-02-18 14:26:42 +0100

    Cinema open again

--- Seven Deadly Apps/Assets/Scripts/Misc/LightFlicker.cs
+++ Seven Deadly Apps/Assets/Scripts/Misc/LightFlicker.cs
@@ -9,7 +9,11 @@ public class LightFlicker : MonoBehaviour
     public float flickerInterval = 0.5f;
 
     private float nextFlickerTime;
-    private Color[] flickerColors = { new Color(1f, 1f, 1f, 0.5f), new Color(0f, 0f, 0f, 0.5f), new Color(0.464f, 0.141f, 0.141f, 0.5f)};
+    private Color[] flickerColors = {
+        new Color(1f, 1f, 1f, 0.5f),     // White with 50% opacity
+        new Color(0.464f, 0.141f, 0.141f, 0.5f), // Red with 50% opacity
+        new Color(0f, 0.008f, 0.106f)    // Dark blue
+    };
 
     private void Start()
     {
