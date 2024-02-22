bbde98153e9b5a4e044182f7f7c8ffb0f1bb44cf
Imated <lineborder122@gmail.com>
2024-02-21 20:06:06 -0500

    Finger In Menu

--- Seven Deadly Apps/Assets/Scripts/Player/MainMenuController.cs
+++ Seven Deadly Apps/Assets/Scripts/Player/MainMenuController.cs
@@ -1,17 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class MainMenuController : MonoBehaviour
 {
-
+    [SerializeField] private Transform cursor;
     public CanvasGroup OptionPanel;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.GetComponent<RectTransform>(), Input.mousePosition, Camera.main, out var localPoint);
+        cursor.localPosition = localPoint;
+    }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Cursor.visible = true;
     }
 
     public void Options()
